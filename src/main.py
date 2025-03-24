e06b79a1a06db51d4f6e8353aeb3af77337efde5
Ohne-Dich <lawerenz.f@gmail.com>
2025-03-24 22:34:54 +0100

    some additions

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print ("hello, world")
+from textnode import TextNode 
+
+def main():
+    test = TextNode("this is a test", "LINK", "www.google.de")
+    print (repr(test))
+
+main()
+
--- src/textnode.py
+++ src/textnode.py
@@ -18,3 +18,8 @@ class TextNode:
     def __eq__ (self, other):
         return self.text == other.text and self.text_type == other.text_type and self.url == other.url
     
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url}"
+    
+
+    
